00b5c3f537abf84eb98b1797866f3277d87341f5
Nehemias Ulloa <nulloa1@iastate.edu>
2016-11-08 21:49:27 -0600

    Added two files

---
+++ GetFluData.R
@@ -0,0 +1,13 @@
+source('delphi_epidata.R')
+res <- Epidata$fluview(list('nat'), list(200040,200041))
+cat(paste(res$result, res$message, length(res$epidata), "\n"))
+res$epidata
+
+res <- as.data.frame(matrix(unlist(res$epidata), ncol=14, byrow=TRUE))
+colnames(res) <- c("release_date","region","issue","epiweek",
+                   "lag","num_ili","num_patients","num_providers",
+                   "num_age_0","num_age_1","num_age_2","num_age_5",
+                   "wili","ili")
+res
+
+get_flu_data("national", c("ilinet"), years=2000)

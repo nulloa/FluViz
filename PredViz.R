e16085aab7a04651902931eff562ddcff72d45ab
Nehemias Ulloa <nulloa1@iastate.edu>
2016-11-19 15:35:21 -0600

    Allow to grab csv from github

--- PredViz.R
+++ PredViz.R
@@ -1,7 +1,8 @@
 library(ggplot2)
 library(gridExtra)
 
-dat <- read.csv("EW44-ISU-2016-11-14.csv", header=TRUE)
+#dat <- read.csv("EW44-ISU-2016-11-14.csv", header=TRUE)
+dat <- read.csv("https://raw.githubusercontent.com/NLMichaud/hierarchicalSIRMods/master/currentSeasonPredictions/submittedCSVs/EW43-ISU-2016-11-07.csv?token=AFk8HZWP_L5HeKKUF7S0jDnIPXBIpHWiks5YOf6qwA%3D%3D", header=TRUE)
 dat$location <- factor(dat$location,
                          levels=c("HHS Region 1","HHS Region 2","HHS Region 3",
                                   "HHS Region 4","HHS Region 5","HHS Region 6",

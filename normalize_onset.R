d28540a7a4a01d8a0419fc32274be8ddd2e3453c
Nehemias Ulloa <nulloa1@iastate.edu>
2016-12-05 14:48:58 -0600

    Add fixes to check if na prob was less than nalim

--- normalize_onset.R
+++ normalize_onset.R
@@ -3,9 +3,16 @@ normalize_onset <- function(data, nalim=0.1){
   for(i in 1:length(region)){
     subreg <- subset(data, location==region[i] & target=="Season onset" & type=="Bin")
     subreg$bin_start_incl <- factor(subreg$bin_start_incl, levels=paste(c(40:52,1:20)))
-    subreg$value[is.na(subreg$bin_start_incl)] <- nalim
-    subreg$value[!is.na(subreg$bin_start_incl)] <- subreg$value[!is.na(subreg$bin_start_incl)]/sum(subreg$value[!is.na(subreg$bin_start_incl)])
-    data[which(data$location==region[i] & data$target=="Season onset" & data$type=="Bin"),"value"] <- subreg$value
+    if(sum(subreg$value[!is.na(subreg$bin_start_incl)]) == 1){
+      data[which(data$location==region[i] & data$target=="Season onset" & data$type=="Bin"),"value"] <- subreg$value
+    }else if(subreg$value[is.na(subreg$bin_start_incl)] < nalim){
+      data[which(data$location==region[i] & data$target=="Season onset" & data$type=="Bin"),"value"] <- subreg$value
+    }else{
+      diff <- subreg$value[is.na(subreg$bin_start_incl)] - nalim
+      subreg$value[is.na(subreg$bin_start_incl)] <- nalim
+      subreg$value[!is.na(subreg$bin_start_incl)] <- subreg$value[!is.na(subreg$bin_start_incl)] + (diff)/length(subreg$value[!is.na(subreg$bin_start_incl)])
+      data[which(data$location==region[i] & data$target=="Season onset" & data$type=="Bin"),"value"] <- subreg$value 
+    }
   }
   
   return(data)

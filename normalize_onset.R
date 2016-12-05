de8845c80b9394618eefbe69521732a8a2f1116e
Nehemias Ulloa <nulloa1@iastate.edu>
2016-12-05 13:25:12 -0600

    Fix naming issues

--- normalize_onset.R
+++ normalize_onset.R
@@ -1,11 +1,11 @@
 normalize_onset <- function(data, nalim=0.1){
-  region <- levels(dat$location)
+  region <- levels(data$location)
   for(i in 1:length(region)){
-    subreg <- subset(dat, location==region[i] & target=="Season onset" & type=="Bin")
+    subreg <- subset(data, location==region[i] & target=="Season onset" & type=="Bin")
     subreg$bin_start_incl <- factor(subreg$bin_start_incl, levels=paste(c(40:52,1:20)))
     subreg$value[is.na(subreg$bin_start_incl)] <- nalim
     subreg$value[!is.na(subreg$bin_start_incl)] <- subreg$value[!is.na(subreg$bin_start_incl)]/sum(subreg$value[!is.na(subreg$bin_start_incl)])
-    dat[which(dat$location==region[i] & dat$target=="Season onset" & dat$type=="Bin"),"value"] <- subreg$value
+    data[which(data$location==region[i] & data$target=="Season onset" & data$type=="Bin"),"value"] <- subreg$value
   }
   
   return(data)

bdc1ce9c93b27690bbd240585a023925eec7a649
Nehemias Ulloa <nulloa1@iastate.edu>
2016-12-05 11:01:48 -0600

    Add verticle line for current ILI percentage

--- plotgrid.R
+++ plotgrid.R
@@ -5,6 +5,11 @@ plotgrid <- function(dat, wk, ilimax, normal=FALSE){
   require(cdcfluview)
   source("get_flu_data_update.R")
   
+  NatFluDat <- get_flu_data("national", NA, "ilinet")
+  RegFluDat <- get_flu_data("hhs", 1:10, "ilinet")
+  
+  CurrentILIPer <- c(tail(as.numeric(RegFluDat$X..WEIGHTED.ILI)/100, n=10),tail(as.numeric(NatFluDat$X..WEIGHTED.ILI)/100, n=1))
+  
   region <- levels(dat$location)
   sbdat <- subset(dat, as.numeric(as.character(bin_start_incl)) <= ilimax & 
                     target %in% c("1 wk ahead","2 wk ahead","3 wk ahead","4 wk ahead"))
@@ -12,18 +17,24 @@ plotgrid <- function(dat, wk, ilimax, normal=FALSE){
   pdf(paste("PredPlots/week", wk, ".pdf", sep=""), width=18, height=12)
 
   for(i in 1:length(region)){
+    # Plot ILI% predictions for Weeks out
     p1wk  <- ggplot(data=subset(dat, location==region[i] & as.numeric(as.character(bin_start_incl)) <= ilimax & target=="1 wk ahead"), 
-                    aes(x=as.numeric(as.character(bin_start_incl)), y=value)) + geom_point() + labs(title = "1 Week Ahead", x="ILI%", y="Prob")
+                    aes(x=as.numeric(as.character(bin_start_incl)), y=value)) + geom_point() + 
+                    labs(title = "1 Week Ahead", x="ILI%", y="Prob") + geom_vline(xintercept = CurrentILIPer[i])
     
     p2wk  <- ggplot(data=subset(dat, location==region[i] & as.numeric(as.character(bin_start_incl)) <= ilimax & target=="2 wk ahead"), 
-                    aes(x=as.numeric(as.character(bin_start_incl)), y=value)) + geom_point() + labs(title = "2 Week Ahead", x="ILI%", y="Prob")
+                    aes(x=as.numeric(as.character(bin_start_incl)), y=value)) + geom_point() + 
+                    labs(title = "2 Week Ahead", x="ILI%", y="Prob") + geom_vline(xintercept = CurrentILIPer[i])
     
     p3wk  <- ggplot(data=subset(dat, location==region[i] & as.numeric(as.character(bin_start_incl)) <= ilimax & target=="3 wk ahead"), 
-                    aes(x=as.numeric(as.character(bin_start_incl)), y=value)) + geom_point() + labs(title = "3 Week Ahead", x="ILI%", y="Prob")
+                    aes(x=as.numeric(as.character(bin_start_incl)), y=value)) + geom_point() + 
+                    labs(title = "3 Week Ahead", x="ILI%", y="Prob") + geom_vline(xintercept = CurrentILIPer[i])
     
     p4wk  <- ggplot(data=subset(dat, location==region[i] & as.numeric(as.character(bin_start_incl)) <= ilimax & target=="4 wk ahead"), 
-                    aes(x=as.numeric(as.character(bin_start_incl)), y=value)) + geom_point() + labs(title = "4 Week Ahead", x="ILI%", y="Prob")
+                    aes(x=as.numeric(as.character(bin_start_incl)), y=value)) + geom_point() + 
+                    labs(title = "4 Week Ahead", x="ILI%", y="Prob") + geom_vline(xintercept = CurrentILIPer[i])
     
+    # Plot Onset Week Predictions
     onstdat <- subset(dat, location==region[i] & target=="Season onset" & type=="Bin")
     onstdat$bin_start_incl <- factor(onstdat$bin_start_incl, levels=paste(c(40:52,1:20)))
     
@@ -37,22 +48,26 @@ plotgrid <- function(dat, wk, ilimax, normal=FALSE){
         geom_point() + labs(title = "Season Onset", x="Week", y="Prob")
     }
     
+    # Plot Peak Percentage Predictions
     pkper <- ggplot(data=subset(dat, location==region[i] & target=="Season peak percentage" & type=="Bin"), 
                     aes(x=as.numeric(as.character(bin_start_incl)), y=value)) + geom_point() + labs(title = "Season Peak Percentage", x="Percent", y="Prob")
     
+    # Plot Peak Week Predictions
     pkwkdat <- subset(dat, location==region[i] & target=="Season peak week")
     pkwkdat$bin_start_incl <- factor(pkwkdat$bin_start_incl, levels=paste(c(40:52,1:20)))
-    
     pkwk  <- ggplot(data=pkwkdat, aes(x=bin_start_incl, y=value)) + 
       geom_point() + ylim(0, 1) + labs(title = "Season Peak Week", x="Week", y="Prob")
     
+    # Arranges various plots onto a grid
     grid.arrange(p1wk,onst,p2wk,pkper,p3wk,pkwk,p4wk, nrow=4, ncol=2, top=paste(region[i]))
   }
   
   
   # Plots the probabilities of %ILI by region colored by week
   natplot <- ggplot(data=sbdat, aes(x=as.numeric(as.character(bin_start_incl)), y=value, color=target)) +
-    geom_point(size = 1) + facet_grid(location~.) + labs(x="ILI%", y="Prob", title="ILI by Region")
+    geom_point(size = 1) + facet_grid(location~.) + labs(x="ILI%", y="Prob", title="ILI by Region") +
+    geom_vline(xintercept = CurrentILIPer[11])
+  
   print(natplot)
   
   dev.off()

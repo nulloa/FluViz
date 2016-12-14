af0c8bed51ff26e6cf9c33246a1dade4a5983e07
Nehemias Ulloa <nulloa1@iastate.edu>
2016-12-13 18:35:21 -0600

    Change nalim to .1%

--- normalize_submission.R
+++ normalize_submission.R
@@ -2,7 +2,7 @@ library("FluSight")
 source("normalize_onset.R")
 
 dat <- read.csv("https://raw.githubusercontent.com/NLMichaud/hierarchicalSIRMods/master/currentSeasonPredictions/submittedCSVs/EW48-ISU-2016-12-12.csv?token=AFk8HQV20wboes4cSdTxk_uYPyDAlClEks5YWJ60wA%3D%3D", header=TRUE)
-normdat <- normalize_onset(dat)
+normdat <- normalize_onset(dat, nalim=.001)
 
 write.csv(normdat, file="temp.csv", row.names=FALSE, col.names=FALSE)
 entryDataFrame <- read_entry("temp.csv")

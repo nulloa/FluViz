d9e81b7a1e2cc5d842d4fa5841e6823442fe3b27
Nehemias Ulloa <nulloa1@iastate.edu>
2016-12-19 09:33:31 -0600

    Update for Week 49

--- normalize_submission.R
+++ normalize_submission.R
@@ -1,7 +1,7 @@
 library("FluSight")
 source("normalize_onset.R")
 
-dat <- read.csv("https://raw.githubusercontent.com/NLMichaud/hierarchicalSIRMods/master/currentSeasonPredictions/submittedCSVs/EW48-ISU-2016-12-12.csv?token=AFk8HQV20wboes4cSdTxk_uYPyDAlClEks5YWJ60wA%3D%3D", header=TRUE)
+dat <- read.csv("https://raw.githubusercontent.com/NLMichaud/hierarchicalSIRMods/master/currentSeasonPredictions/submittedCSVs/EW49-ISU-2016-12-18.csv?token=AFk8HdoUwEWxlm0gvYyw-oeQUZGMPXLYks5YYJFrwA%3D%3D", header=TRUE)
 normdat <- normalize_onset(dat, nalim=.001)
 
 write.csv(normdat, file="temp.csv", row.names=FALSE, col.names=FALSE)

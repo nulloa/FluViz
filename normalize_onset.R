b8945394b44b745c47c4ecc05cee4692c35257f7
Nehemias Ulloa <nulloa1@iastate.edu>
2016-12-26 20:06:39 -0600

    Fix factor issues for bins

--- normalize_onset.R
+++ normalize_onset.R
@@ -2,7 +2,9 @@ normalize_onset <- function(data, nalim=0.1){
   region <- levels(data$location)
   for(i in 1:length(region)){
     subreg <- subset(data, location==region[i] & target=="Season onset" & type=="Bin")
-    subreg$bin_start_incl <- factor(subreg$bin_start_incl, levels=paste(c(40:52,1:20)))
+    subreg$bin_start_incl <- factor(as.factor(subreg$bin_start_incl))
+    subreg$bin_start_incl <- factor(subreg$bin_start_incl, levels=c(paste(c(40:52,1:20),".0",sep=""),"none"))
+    subreg$bin_start_incl <- c(paste(c(40:52,1:20)),NA)
     if(sum(subreg$value[!is.na(subreg$bin_start_incl)]) == 1){
       data[which(data$location==region[i] & data$target=="Season onset" & data$type=="Bin"),"value"] <- subreg$value
     }else if(subreg$value[is.na(subreg$bin_start_incl)] < nalim){

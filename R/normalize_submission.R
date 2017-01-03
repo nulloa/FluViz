3b1987ddaef25d210f008fb0bbc6172461898f73
Nehemias Ulloa <nulloa1@iastate.edu>
2017-01-03 09:52:47 -0600

    Correct source file location

--- R/normalize_submission.R
+++ R/normalize_submission.R
@@ -1,7 +1,7 @@
 require("FluSight")
-source("normalize_onset.R")
+source("R/normalize_onset.R")
 
-dat <- read.csv("https://raw.githubusercontent.com/NLMichaud/hierarchicalSIRMods/master/currentSeasonPredictions/submittedCSVs/EW50-ISU-2016-12-27.csv?token=AFk8HTS07BUJLihbjx_XZHXudv_qK7ruks5YbDthwA%3D%3D", header=TRUE)
+dat <- read.csv("https://raw.githubusercontent.com/NLMichaud/hierarchicalSIRMods/master/currentSeasonPredictions/submittedCSVs/EW51-ISU-2017-01-03.csv?token=AFk8HZotqLeuNjvWs0-PDrgsEf1hK2yGks5YdPyVwA%3D%3D", header=TRUE)
 normdat <- normalize_onset(dat, nalim=.001)
 
 write.csv(normdat, file="temp.csv", row.names=FALSE, col.names=FALSE)

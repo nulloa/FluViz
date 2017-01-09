5e2cdd6077a583771823efba894dc69397e98fa8
Nehemias Ulloa <nulloa1@iastate.edu>
2017-01-09 15:41:14 -0600

    Change data read in to plotviz

--- R/normalize_submission.R
+++ R/normalize_submission.R
@@ -1,7 +1,8 @@
 require("FluSight")
 source("R/normalize_onset.R")
 
-dat <- read.csv("https://raw.githubusercontent.com/NLMichaud/hierarchicalSIRMods/master/currentSeasonPredictions/submittedCSVs/EW51-ISU-2017-01-03.csv?token=AFk8HZotqLeuNjvWs0-PDrgsEf1hK2yGks5YdPyVwA%3D%3D", header=TRUE)
+url <- "https://raw.githubusercontent.com/NLMichaud/hierarchicalSIRMods/master/currentSeasonPredictions/submittedCSVs/EW52-ISU-2017-01-08.csv?token=AFk8HfgQYL5Z8Dp43F1KCqYuGP277twQks5YfS1hwA%3D%3D"
+dat <- read.csv(url, header=TRUE)
 normdat <- normalize_onset(dat, nalim=.001)
 
 write.csv(normdat, file="temp.csv", row.names=FALSE, col.names=FALSE)

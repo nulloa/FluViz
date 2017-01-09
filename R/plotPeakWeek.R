0461d3ecfb6ead1b70d82e4de29d451a2948acc3
Nehemias Ulloa <nulloa1@iastate.edu>
2017-01-09 15:38:23 -0600

    Rename the script

--- R/plotPeakWeek.R
+++ R/plotPeakWeek.R
@@ -5,6 +5,6 @@ plotPeakWeek <- function(dat, region){
   
   d$bin_start_incl <- factor(factor(as.factor(d$bin_start_incl)), levels=c(40:52,1:20))
   
-  ggplot(data=pkwkdat, aes(x=bin_start_incl, y=value)) + 
+  ggplot(data=d, aes(x=bin_start_incl, y=value)) + 
     geom_point() + ylim(0, 1) + labs(title = "Season Peak Week", x="Week", y="Prob")
 }

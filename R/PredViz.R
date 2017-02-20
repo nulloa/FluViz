ae672ae60d52205f10fd1b2ddd2b7eb506720a16
Nehemias Ulloa <nulloa1@iastate.edu>
2017-02-19 22:28:54 -0600

    Update for week 6 and use file choose to get csv

--- R/PredViz.R
+++ R/PredViz.R
@@ -3,12 +3,12 @@ library(gridExtra)
 library(httr)
 library(pbapply)
 library(cdcfluview)
+library(MMWRweek)
 source("R/normalize_onset.R") # Load in normalize fn
 source("R/plotgrid.R") # Load in plot fn
 
 # Read in data
-url <- "https://raw.githubusercontent.com/NLMichaud/hierarchicalSIRMods/master/currentSeasonPredictions/submittedCSVs/EW05-ISU-2017-02-13.csv?token=AFk8Hf1g2cOP5eV9f5c5DKpvwKxpTgOsks5Yq6SVwA%3D%3D"
-dat <- read.csv(url, header=TRUE)
+dat <- read.csv(file.choose(), header=TRUE)
 dat$location <- factor(dat$location,
                          levels=c("HHS Region 1","HHS Region 2","HHS Region 3",
                                   "HHS Region 4","HHS Region 5","HHS Region 6",
@@ -16,7 +16,7 @@ dat$location <- factor(dat$location,
                                   "HHS Region 10","US National"))
 
 # Create Plots
-plotgrid(normalize_onset(dat, nalim=.001), week="5_Normalized", ilimx=11)
-plotgrid(dat, week=5, ilimx=11)
+plotgrid(normalize_onset(dat, nalim=.001), week="6_Normalized", ilimx=12)
+plotgrid(dat, week=6, ilimx=12)
 
 
